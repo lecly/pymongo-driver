e5702163fd259893bc009317a03ebf297fb0f99c
Combo <combo@desvp.com>
2015-07-07 16:56:02 +0800

    set __version__ to 0.0.3

--- pymongo_driver/__init__.py
+++ pymongo_driver/__init__.py
@@ -4,7 +4,7 @@
 from .base import ObjectId, RequireField, RequireFieldError
 
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 __all__ = ['driver', 'ObjectId', 'RequireField', 'RequireFieldError']
 
